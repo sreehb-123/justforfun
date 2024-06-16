51d2c8469131d2d07035ba9edaef048b2b378309
sreehb-123 <152023996+sreehb-123@users.noreply.github.com>
2024-06-16 12:45:13 +0530

    Update first.js

--- first.js
+++ first.js
@@ -3,7 +3,7 @@ let userNum = prompt("Guess the number: ");
 
 while(userNum!=gameNum){
     if(userNum<gameNum){
-        userNum = prompt("Oops! You are behind the correct number...Guess again");
+        userNum = prompt("Oops! You are still behind the correct number...Guess again");
     }
     else{
         userNum = prompt("Oops! You are ahead of the correct number...Guess again");
